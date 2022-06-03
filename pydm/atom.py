686d512cd5c9440421f0bd30c614ce5fa41b1d05
Kyle Oliver <oliverke@mail.uc.edu>
2022-06-03 19:12:55 -0400

    More framework

--- pydm/atom.py
+++ pydm/atom.py
@@ -2,6 +2,9 @@
 
 class Atom:
     """The Atom class is the base class for all objects in the game"""
+    _procs = None
+    _vars = None
+    _verbs = None
     def __init__(self, *args, **kwargs):
         pass
 
---
+++ pydm/mixins/pathable.py
@@ -0,0 +1,6 @@
+
+
+class Pathable:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
